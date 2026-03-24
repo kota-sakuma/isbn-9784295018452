42a6f1b58bcbb5500316c8a3702a61d9ef4a0985
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-24 16:49:36 +0900

    feat: Steam API による処理を追加

--- lambda/Main.java
+++ lambda/Main.java
@@ -1,17 +1,42 @@
 package lambda;
 
+import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
-    BiFunction<Integer, String, String> passCheck =
-        (point, name) -> name + " is " + (point > 65 ? "passed" : "failed");
+  private static final int PASS_SCORE = 65;
+  private static final int MIN_NAME_LENGTH = 4;
+  private static final List<String> MEMBERS = List.of("John", "Jane", "Jimmy", "Jill");
 
-    System.out.println(isOdd.apply(1));
+  enum ExamResult {
+    PASSED("passed"),
+    FAILED("failed");
+
+    private final String result;
+
+    private ExamResult(String result) {
+      this.result = result;
+    }
+
+    public String getResult() {
+      return result;
+    }
+  }
+
+  public static void main(String[] args) {
+    Predicate<Integer> isOdd = x -> x % 2 == 1;
+    BiFunction<Integer, String, String> passCheck =
+        (point, name) ->
+            name
+                + " is "
+                + (point >= PASS_SCORE ? ExamResult.PASSED : ExamResult.FAILED).getResult();
+
+    System.out.println(isOdd.test(1));
     System.out.println(passCheck.apply(65, "John"));
     System.out.println(passCheck.apply(80, "Jane"));
+
+    MEMBERS.stream().filter(name -> name.length() >= MIN_NAME_LENGTH).forEach(System.out::println);
   }
 }
