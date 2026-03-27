4e9f6be1507163ffb15ef9dba3c50a47233a4bb5
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-27 10:47:23 +0900

    feat: use buffering to write data

--- FileStream.java
+++ FileStream.java
@@ -1,34 +1,34 @@
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 class FileStream {
 
   public static void main(String[] args) {
-    // Validate the arguments
+    validateArguments(args);
+
+    copyFile(args[0], args[1]);
+  }
+
+  private static void validateArguments(String[] args) {
     if (args.length != 2) {
-      System.out.println("Usage: java FileStream <input file> <output file>");
-      System.exit(1);
+      throw new IllegalArgumentException("Usage: java FileStream <input file> <output file>");
     }
-
-    // Get the input and output files
-    String inputFile = args[0];
-    String outputFile = args[1];
-
-    // Copy the file
-    copyFile(inputFile, outputFile);
   }
 
   private static void copyFile(String inputFile, String outputFile) {
     try (FileInputStream in = new FileInputStream(inputFile);
-        FileOutputStream out = new FileOutputStream(outputFile)) {
+        FileOutputStream out = new FileOutputStream(outputFile);
+        GZIPOutputStream gzipOut = new GZIPOutputStream(out);
+        BufferedOutputStream bufferedOut = new BufferedOutputStream(gzipOut)) {
       int data;
       while ((data = in.read()) != -1) {
-        out.write(data);
+        bufferedOut.write(data);
       }
     } catch (IOException e) {
-      System.out.println("Error copying file: " + e.getMessage());
-      System.exit(1);
+      throw new RuntimeException(e);
     }
   }
 }
